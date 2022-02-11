374733d79db8fe931659b22ccf2e9f4370a4cadc
Gyunseo Lee <rbstj0311@naver.com>
2022-02-12 01:12:02 +0900

    On branch master - set view engine and loaded assets - added new files above - editing index.ejs

--- server.js
+++ server.js
@@ -19,6 +19,11 @@ app.use(bodyparser.urlencoded({ extended : true }));
 app.set('view engine', 'ejs');
 // app.set('views', path.resolve(__dirname, 'views/ejs'));
 
+//load assets
+app.use('/css', express.static(path.resolve(__dirname, 'assets/css')));
+app.use('/img', express.static(path.resolve(__dirname, 'assets/img')));
+app.use('/js', express.static(path.resolve(__dirname, 'assets/js')));
+//css/style.css
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}\n`);
 });
