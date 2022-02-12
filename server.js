9fa39923caf98b0d8bb6f9a41dc705b2b513cad8
Gyunseo Lee <rbstj0311@naver.com>
2022-02-13 00:08:57 +0900

    On branch master - installed prettier, eslint - added router.js

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const morgan = require("morgan");
-const bodyparser = require("body-parser");
-const path = require("path");
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
-dotenv.config({ path: "config.env" });
+dotenv.config({ path: 'config.env' });
 const PORT = process.env.PORT || 8080;
 
-//log requests
-app.use(morgan("tiny"));
+// log requests
+app.use(morgan('tiny'));
 
-//parse request to body-parser
+// parse request to body-parser
 app.use(bodyparser.urlencoded({ extended: true }));
 
-//set view engine
-app.set("view engine", "ejs");
+// set view engine
+app.set('view engine', 'ejs');
 // app.set('views', path.resolve(__dirname, 'views/ejs'));
 
-//load assets
-app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
-app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
-app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
-//css/style.css
+// load assets
+app.use('/css', express.static(path.resolve(__dirname, 'assets/css')));
+app.use('/img', express.static(path.resolve(__dirname, 'assets/img')));
+app.use('/js', express.static(path.resolve(__dirname, 'assets/js')));
+// css/style.css
+
+// load routers
+app.use('/', require('./server/routes/router'));
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}\n`);
-});
-
-app.get("/", (req, res) => {
-  res.render("index");
 });
