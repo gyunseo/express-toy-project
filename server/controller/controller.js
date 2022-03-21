457648d02f05855aebe92c79896b13f4b222a1e7
Gyunseo Lee <rbstj0311@naver.com>
2022-03-21 23:27:40 +0900

    deleted:    .eslintrc.json 	modified:   .prettierignore 	modified:   package-lock.json 	modified:   package.json 	new file:   server/controller/controller.js 	new file:   server/model/model.js 	modified:   server/routes/router.js 	modified:   views/include/_footer.ejs     - deleted eslint     - tested POST API, success

---
+++ server/controller/controller.js
@@ -0,0 +1,40 @@
+var Userdb = require('../model/model');
+
+// create and save new user
+exports.create = (req, res) => {
+  // validate request
+  if (!req.body) {
+    res.status(400).send({ message: 'Content cannot be empty!' });
+    return;
+  }
+
+  // new user
+  const user = new Userdb({
+    name: req.body.name,
+    email: req.body.email,
+    gender: req.body.gender,
+    status: req.body.status,
+  });
+
+  // save user in the database
+  user
+    .save(user)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || 'Some error occured while creating a create operation',
+      });
+    });
+};
+
+// retrieve and return all users/ retrieve and return a single user
+exports.find = (req, res) => {};
+
+// update a new identified user by user id
+exports.update = (req, res) => {};
+
+// delete a user with specified user id in the request
+exports.delete = (req, res) => {};
--- server/routes/router.js
+++ server/routes/router.js
@@ -1,7 +1,9 @@
 const express = require('express');
 
 const route = express.Router();
+
 const services = require('../services/render');
+const controller = require('../controller/controller');
 
 /**
  * @description Root route
@@ -20,5 +22,11 @@ route.get('/add-user', services.add_user);
  * @method GET /update-user
  */
 route.get('/update-user', services.update_user);
+
+//API
+route.post('/api/users', controller.create);
+route.get('/api/users', controller.find);
+route.put('/api/users/:id', controller.update);
+route.post('/api/users/:id', controller.delete);
 
 module.exports = route;
