55a2600e0eca6de37ef3e799461d7df33ee441eb
Gyunseo Lee <rbstj0311@naver.com>
2022-03-26 00:13:43 +0900

    On branch master  Your branch is ahead of 'origin/master' by 1 commit.  Changes to be committed: 	modified:   assets/js/index.js 	modified:   views/include/_show.ejs - user delete function completed

--- assets/js/index.js
+++ assets/js/index.js
@@ -26,3 +26,23 @@ $('#update_user').submit(function (event) {
     alert('Data updated successfully!');
   });
 });
+
+if (window.location.pathname == '/') {
+  $ondelete = $('.table tbody td a.delete');
+  $ondelete.click(function () {
+    let id;
+    let request;
+    id = $(this).attr('data-id');
+    request = {
+      url: `http://localhost:3000/api/users/${id}`,
+      method: 'DELETE',
+    };
+
+    if (confirm('Do you really want to delete this record?')) {
+      $.ajax(request).done((response) => {
+        alert('Data deleted successfully!');
+        location.reload();
+      });
+    }
+  });
+}
