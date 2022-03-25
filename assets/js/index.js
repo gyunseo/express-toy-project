5bacfe8ad172255df068784f826978165ea83aa0
Gyunseo Lee <rbstj0311@naver.com>
2022-03-26 00:05:59 +0900

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   assets/js/index.js 	modified:   views/update_user.ejs - update user function completed

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,26 @@ $('#add_user').submit((event) => {
 });
 
 $('#update_user').submit(function (event) {
-  let unindexed_array = $(this).serializeArray();
+  let unindexed_array;
+  let data;
+  let request;
+
   event.preventDefault();
+
+  unindexed_array = $(this).serializeArray();
   console.log(unindexed_array);
+  data = {};
+  $.map(unindexed_array, (n, i) => {
+    data[n['name']] = n['value'];
+  });
+  console.log(data);
+  request = {
+    url: `http://localhost:3000/api/users/${data.id}`,
+    method: 'PUT',
+    data: data,
+  };
+
+  $.ajax(request).done((response) => {
+    alert('Data updated successfully!');
+  });
 });
